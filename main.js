b0b7ad05e08968f652ef345b980e827eae892596
YxngXi1 <147933670+YxngXi1@users.noreply.github.com>
2023-10-16 13:23:14 -0400

    start of the project

---
+++ main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const h1 = document.querySelector('h1'); // Select the first h1 element
+    h1.addEventListener('click', () => {
+        alert('Hi I\'m Yang!');
+    });
+}); 
